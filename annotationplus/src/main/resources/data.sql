3b50163bb5e23ac76bae1dd46f45692559c528e3
DESKTOP-4AOQFUJ\Semer <lyubomir.semerdzhiev@gmail.com>
2020-11-18 20:15:15 +0200

    Update sql catch exception

--- annotationplus/src/main/resources/data.sql
+++ annotationplus/src/main/resources/data.sql
@@ -1,4 +1,6 @@
+SET FOREIGN_KEY_CHECKS=0;
 REPLACE INTO `role` VALUES (1,'ADMIN');
+SET FOREIGN_KEY_CHECKS=1;
 alter table `source` modify `content` LONGTEXT;
 alter table `source` modify `name` LONGTEXT;
 alter table `annotation` modify `content` LONGTEXT;
--- annotationplus/src/main/resources/static/js/redirect.js
+++ annotationplus/src/main/resources/static/js/redirect.js
@@ -1,16 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var xhr;
     var _orgAjax = $.ajaxSettings.xhr;
-    $.ajaxSettings.xhr = function() {
+    $.ajaxSettings.xhr = function () {
         xhr = _orgAjax();
         return xhr;
     };
 
-    $(document).ajaxComplete(function() {
-        if (xhr.responseURL.indexOf("/login") > -1) {
-            $(".formContainer").css("display","none");
-            $("#sessionExpiredContainer").css("display","block");
-            window.open("/login", "_blank");
+    $(document).ajaxComplete(function () {
+        try {
+            if (xhr.responseURL.indexOf("/login") > -1) {
+                $(".formContainer").css("display", "none");
+                $("#sessionExpiredContainer").css("display", "block");
+                window.open("/login", "_blank");
+            }
+        } catch (e) {
+
         }
     });
 });
